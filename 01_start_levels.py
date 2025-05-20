075b907204f5c1e6e397ed2d5387e902af460630
shortalligator <101073123+shortalligator@users.noreply.github.com>
2025-05-20 13:29:24 +1200

    Add files via upload
    added buttons

--- 01_start_levels.py
+++ 01_start_levels.py
@@ -9,18 +9,37 @@ class Levels:
         self.level_frame = Frame(padx=10, pady=10, bg="#cce5ff")
         self.level_frame.grid()
 
-        levels_labels_list = [
+        # list for the heading labels (text | font)
+        headings_labels_list = [
             ["Linear Algebra Quiz", ("Arial", "18", "bold")],
             ["Select a Level", ("Arial", "16", "bold")]
         ]
 
+        # create the labels
         levels_labels_ref = []
-        for count, item in enumerate(levels_labels_list):
+        for count, item in enumerate(headings_labels_list):
             make_label = Label(self.level_frame, text=item[0], font=item[1],
                                fg="#000000",bg="#cce5ff", justify="left", padx=10, pady=10)
             make_label.grid(row=count)
 
             levels_labels_ref.append(make_label)
+
+        # list for buttons (frame | text | bg | command | width | row)
+        levels_button_list = [
+            [self.level_frame, "EASY", "#FFFB92", "", 20, 3],
+            [self.level_frame,"MEDIUM", "#FFFFFF", "", 20, 4],
+            [self.level_frame, "HARD", "#FFFFFF", "", 20, 5]
+        ]
+
+        # create buttons and add to list
+        control_ref_list = []
+        for item in levels_button_list:
+            make_level_button = Button(item[0], text=item[1], bg=item[2], command=item[3],
+                                         font=("Arial", 16, "bold"),
+                                         width=item[4], fg="#FFFFFF")
+            make_level_button.grid(row=item[5], padx=5, pady=5)
+
+            control_ref_list.append(make_level_button)
 
 
 # main routine
